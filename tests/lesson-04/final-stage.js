5960b1fdd11f615c7a8bde4175aa00305528ac09
test 01 - HoangAnh <anhlh.90@gmail.com>
2026-02-02 17:20:54 +0700

    feat: add final stage for challenge 05

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,16 @@
+// Stage 05: Đếm và in ra có bao nhiêu các cặp số từ 1 - 100 có tổng chia hết cho 17
+function findPairsDivisibleBy17() {
+    let count = 0;
+    for (let i = 1; i <= 100; i++) {
+        for (let j = i+1; j <= 100; j++) {
+            let sum = i + j;
+
+            if (sum % 17 === 0) {
+                console.log(`(${i}, ${j}) = ${sum}`)
+                count += 1;
+            }
+        }
+    }
+console.log(`\n Tổng cộng: ${count} cặp`);
+}
+findPairsDivisibleBy17();
